ede54522bbf769ad36d19b7e6b6390e9bc735f04
Stefan Frei <stefan.a.frei@bluewin.ch>
2017-01-21 16:40:33 +0100

    Add project UnitTest_BlackJack

---
+++ UnitTest_BlackJack/src/Deck_Test.h
@@ -0,0 +1,18 @@
+/*
+ * Deck_test.h
+ *
+ *  Created on: 21.01.2016
+ *      Author: Stefan
+ */
+
+#ifndef DECK_TEST_H_
+#define DECK_TEST_H_
+#include "gtest/gtest.h"
+
+
+TEST(Deck, yoo)
+{
+	std::cout << "YOO" << std::endl;
+}
+
+#endif
---
+++ UnitTest_BlackJack/src/UnitTest_BlackJack.cpp
@@ -0,0 +1,18 @@
+/*
+ * UnitTest.cpp
+ *
+ *  Created on: 21.01.2017
+ *      Author: Stefan
+ */
+
+#include <iostream>
+#include "gtest/gtest.h"
+#include "Deck_Test.h"
+
+
+
+int main(int argc, char ** argv)
+{ ::testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
